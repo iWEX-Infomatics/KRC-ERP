3a39fe71b502ec622c99d9804a21e361ea91f3aa
imadhubala27 <imadhubala27@gmail.com>
2025-12-10 18:04:57 +0530

    changes

--- krishna_royal_club/krishna_royal_club/doctype/membership_agreement/test_membership_agreement.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Madhu and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestMembershipAgreement(FrappeTestCase):
-	pass
--- krishna_royal_club/krishna_royal_club/doctype/tour_visit/tour_visit.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Madhu and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Tour Visit", {
-// 	refresh(frm) {
-
-// 	},
-// });
